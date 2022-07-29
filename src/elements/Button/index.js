1f4d0380c4ace29398a1287b7520347778d80937
Sutisna <59520599+SUTISNA10@users.noreply.github.com>
2022-07-29 14:36:45 +0700

    Bagian Component Button, Mendeklarasikan variabel className, membuat kondisi untuk push sebuah string dari className, mendeklarasikan Variabel onClick, membuat kondisi sebuah Link eksternal dan internal

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -5,6 +5,34 @@ import propTypes from "prop-types";
 import React from "react";
 
 export default function Button() {
+  const className = [props.className];
+  if (props.isPrimary) className.push("btn-primary");
+  if (props.isLight) className.push("btn-light");
+  if (props.isLarge) className.push("btn-lg");
+  if (props.isSmall) className.push("btn-sm");
+  if (props.isBlock) className.push("btn-block");
+  if (props.hasShadow) className.push("btn-shadow");
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.type === "link") {
+    if (props.isExternal) {
+      return (
+        <a href={props.href} className={className.join(" ")} style={props.style} target={props.target === "_blank" ? "_blank" : undefined} rel={props.target === "_blank" ? "noopener noreferrer" : undefined}>
+          {props.children}
+        </a>
+      );
+    } else {
+      return (
+        <Link to={props.href} className={className.join(" ")} style={props.style} onClick={onClick}>
+          {props.children}
+        </Link>
+      );
+    }
+  }
+
   return <div></div>;
 }
 
